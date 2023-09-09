8f1a57a602f1a6f86c6a161519d056b3f0c52dab
Yu <poscinre@gmail.com>
2023-09-10 00:10:19 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.ts
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf-8').trim();
+const N = parseInt(input);
+
+for (let i = 1; i <= N; i++) {
+    const stars = '*'.repeat(i);
+    console.log(stars);
+}
