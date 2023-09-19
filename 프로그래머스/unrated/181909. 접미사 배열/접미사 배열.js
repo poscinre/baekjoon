792ed0d4291c0dc50323cb110dc97cc1022ef638
Yu <poscinre@gmail.com>
2023-09-20 08:48:14 +0900

    [unrated] Title: 접미사 배열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var answer = [];
+    
+    var length = my_string.length;
+    
+    for (var i = 0; i < length; i++) {
+        answer.push(my_string.slice(i));
+    }
+    
+    answer.sort();
+    
+    return answer;
+}
