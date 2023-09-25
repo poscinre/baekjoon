73249a3889cc45c528415c1bd1bf18ad7b5671b3
Yu <poscinre@gmail.com>
2023-09-26 07:11:20 +0900

    [unrated] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    var answer = 0;
+
+    for (var i = 0; i < num_list.length; i++) {
+        var num = num_list[i];
+
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num = num / 2;
+            } else {
+                num = (num - 1) / 2;
+            }
+            answer++;
+        }
+    }
+
+    return answer;
+}
