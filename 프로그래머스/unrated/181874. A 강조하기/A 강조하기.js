8000879024821deac712a407f4934b3d6f7aeb1e
Yu <poscinre@gmail.com>
2023-09-27 00:12:44 +0900

    [unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,5 @@
+function solution(myString) {
+    return [...myString.replaceAll('a', 'A')]
+        .map(a => (a.charCodeAt() > 65 && a.charCodeAt() < 97) ? a.toLowerCase() : a)
+        .join("");
+}
