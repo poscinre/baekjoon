3eb258e0bcf6d1f7b7e0b35ad1c47cd5dceb3ae5
Yu <poscinre@gmail.com>
2023-10-06 07:21:46 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.36 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    var answer = [];
+    
+    for (var i = 0; i < n; i++) {
+        var row = [];
+        for (var j = 0; j < n; j++) {
+            if (i === j) {
+                row.push(1);
+            } else {
+                row.push(0);
+            }
+        }
+        answer.push(row);
+    }
+    
+    return answer;
+}
