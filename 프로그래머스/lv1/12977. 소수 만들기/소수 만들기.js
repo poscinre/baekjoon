80800f1c0da80287f108ccf5f96eab4fb47fc705
Yu <poscinre@gmail.com>
2023-10-08 06:35:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,27 @@
+function solution(nums) {
+    function isPrime(num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 === 0 || num % 3 === 0) return false;
+        
+        for (let i = 5; i * i <= num; i += 6) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
+        }
+        return true;
+    }
+    
+    let count = 0;
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
