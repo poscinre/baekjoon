0c8d45ba77fbf4683a6202ec75476e91750eebda
Yu <poscinre@gmail.com>
2023-10-10 07:04:22 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
@@ -0,0 +1,9 @@
+function solution(array) {
+    array.sort(function(a, b) {
+        return a - b;
+    });
+
+    var middleIndex = Math.floor(array.length / 2);
+
+    return array[middleIndex];
+}
