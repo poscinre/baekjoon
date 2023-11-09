6a807c46d0adf3047b17c2a2d37823cd3c73766b
Yu <poscinre@gmail.com>
2023-11-10 07:16:30 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.ts
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
