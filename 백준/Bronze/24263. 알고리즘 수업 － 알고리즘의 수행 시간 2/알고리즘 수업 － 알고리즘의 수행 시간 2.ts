19ec75a3986aa40470e91320094bc7e70ad70ad9
Yu <poscinre@gmail.com>
2023-12-15 08:42:00 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.ts
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+const n = parseInt(input[0]);
+
+const numOperations = n;
+const maxDegree = 1;
+
+console.log(numOperations);
+console.log(maxDegree);
