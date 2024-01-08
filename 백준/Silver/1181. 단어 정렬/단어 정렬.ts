082a4b8d9b0a3ccf8cc461871e99ef3f548cbf96
Yu <poscinre@gmail.com>
2024-01-08 13:19:14 +0900

    [Silver V] Title: 단어 정렬, Time: 256 ms, Memory: 19960 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.ts
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+const N = Number(input.shift());
+
+input.sort((a, b) => {
+  return a.length - b.length || a.localeCompare(b);
+});
+
+const set = new Set(input);
+
+console.log(Array.from(set).join("\n"));
