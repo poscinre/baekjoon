bd0421e8a8ba6ee02ae9cbd148a45f6f4be556e7
Yu <poscinre@gmail.com>
2024-03-05 08:24:28 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 1316 ms, Memory: 199536 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.ts
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
+
+let str = "";
+
+function star (i, j) {
+  if(i % 3 === 1 && j % 3 === 1){
+    str += " ";
+  } else {
+    if(Math.floor(i / 3) === 0 && Math.floor(j / 3) === 0){
+      str += "*";
+    } else {
+      star(Math.floor(i / 3), Math.floor(j / 3));
+    }
+  }
+}
+
+for(let i = 0; i < input; i++){
+  for(let j = 0; j < input; j++){
+    star(i, j);
+  }
+  str += "\n";
+}
+
+console.log(str);
